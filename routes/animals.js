f30f80da609befabf9caf15ed617dcd1afa2f773
kronosGR <geo.elgeo@gmail.com>
2023-09-06 18:49:17 +0200

    changed to sequelize raw sql

--- routes/animals.js
+++ routes/animals.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const isMember = require('../middleware/isMember');
+const isAdmin = require('../middleware/isAdmin');
 
 const db = require('../models');
 const AnimalsService = require('../services/AnimalsService');
@@ -8,22 +10,24 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const animalsService = new AnimalsService(db);
 
-router.get('/', async function (req, res, next) {
-  let animals = await animalsService.getAnimals();
+router.get('/:which', async function (req, res, next) {
+  let animals;
+  if (req.params.which === 'all') animals = await animalsService.getAnimals();
+
   animals.url = getUrl() + 'animals';
 
   if (!req.user) res.render('animals', { user: null, animals: animals });
   else res.render('animals', { user: req.user, animals: animals });
 });
 
-router.post('/', jsonParser, async function (req, res, next) {
+router.post('/', isMember, jsonParser, async function (req, res, next) {
   await animalsService.adoptAnimal(req.body.Id, req.body.UserId);
 
   if (!req.user) res.render('animals', { user: null });
   else res.render('animals', { user: req.user });
 });
 
-router.delete('/', jsonParser, async function (req, res, next) {
+router.delete('/', isAdmin, jsonParser, async function (req, res, next) {
   await animalsService.cancelAdoption(req.body.Id);
 
   if (!req.user) res.render('animals', { user: null });
--- services/AnimalsService.js
+++ services/AnimalsService.js
@@ -2,6 +2,7 @@ const sequelize = require('sequelize');
 
 class AnimalsService {
   constructor(db) {
+    this.sequelize = db.sequelize;
     this.Animal = db.Animal;
     this.Specie = db.Specie;
     this.Size = db.Size;
@@ -9,45 +10,62 @@ class AnimalsService {
   }
 
   async getAnimals() {
-    const animals = await this.Animal.findAll({
-      attributes: [
-        'Name',
-        'id',
-        'BirthDate',
-        'UserId',
-        [
-          sequelize.literal(
-            `DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), BirthDate)), '%Y') + 0`
-          ),
-          'Age',
-        ],
-      ],
-      where: {},
-      order: [['id', 'Asc']],
-      include: [
-        {
-          model: this.Specie,
-          required: true,
-          attributes: ['Name'],
-        },
-        {
-          model: this.Size,
-          required: true,
-          attributes: ['Name'],
-        },
-        {
-          model: this.Temperament,
-          required: true,
-          through: { attributes: [] },
-        },
-      ],
-    });
-    animals.forEach((animal) => {
-      let arr = [];
-      animal.Temperaments.forEach((temperament) => arr.push(temperament.name));
-      animal.Temperament = arr.join(', ');
-    });
-    return animals;
+    const animals = await this.sequelize.query(
+      'SELECT `Animal`.`Name`, `Animal`.`id`, `Animal`.`BirthDate`, `Animal`.`UserId`, ' +
+        "DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), BirthDate)), '%Y') + 0 AS `Age`,  " +
+        '`Specie`.`id` AS `Specie.id`, `Specie`.`Name` AS `SpecieName`,  ' +
+        '`Size`.`id` AS `Size.id`, `Size`.`Name` AS `SizeName`,  ' +
+        'group_concat(`Temperaments`.`id`) AS `Temperaments.id`, ' +
+        ' group_concat(`Temperaments`.`name`) AS `TemperamentsName` ' +
+        '  FROM `Animals` AS `Animal` INNER JOIN `Species` AS  ' +
+        '`Specie` ON `Animal`.`SpecieId` = `Specie`.`id` INNER JOIN `Sizes` ' +
+        ' AS `Size` ON `Animal`.`SizeId` = `Size`.`id` ' +
+        ' INNER JOIN ( `AnimTempes` AS `Temperaments->AnimTempe` INNER JOIN  ' +
+        ' `Temperaments` AS `Temperaments` ON  ' +
+        ' `Temperaments`.`id` = `Temperaments->AnimTempe`.`TemperamentId`) ON ' +
+        '  `Animal`.`id` = `Temperaments->AnimTempe`.`AnimalId` GROUP BY `Animal`.`id` ' +
+        '   ORDER BY `Animal`.`id` ASC '
+    );
+    // const animals = await this.Animal.findAll({
+    //   attributes: [
+    //     'Name',
+    //     'id',
+    //     'BirthDate',
+    //     'UserId',
+    //     [
+    //       sequelize.literal(
+    //         `DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), BirthDate)), '%Y') + 0`
+    //       ),
+    //       'Age',
+    //     ],
+    //   ],
+    //   where: {},
+    //   order: [['id', 'Asc']],
+    //   include: [
+    //     {
+    //       model: this.Specie,
+    //       required: true,
+    //       attributes: ['Name'],
+    //     },
+    //     {
+    //       model: this.Size,
+    //       required: true,
+    //       attributes: ['Name'],
+    //     },
+    //     {
+    //       model: this.Temperament,
+    //       required: true,
+    //       through: { attributes: [] },
+    //     },
+    //   ],
+    // });
+    console.log(animals[0][0]);
+    // animals.forEach((animal) => {
+    //   let arr = [];
+    //   animal.Temperaments.forEach((temperament) => arr.push(temperament.name));
+    //   animal.Temperament = arr.join(', ');
+    // });
+    return animals[0];
   }
 
   async adoptAnimal(id, userId) {
